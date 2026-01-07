db163533692f006d4dadcf7c0d14c093a1b17742
ifskelton69 <adityabirajdar0305@gmail.com>
2026-01-07 12:08:38 +0530

    final push

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,12 @@ import authroutes from "./routes/auth.routes.js";
 import messageroutes from "./routes/message.routes.js";
 import cookieparser from "cookie-parser";
 import cors from 'cors'
+import path from 'path';
 import {app, server} from './lib/socket.js';
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 app.use(cookieparser());
 app.use(express.urlencoded({ extended: true })); // for x-www-form-urlencoded
@@ -23,6 +25,13 @@ app.get('/api/test', (req, res) => {
 });
 app.use('/api/auth', authroutes);
 app.use('/api/messages', messageroutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend" , "dist", "index.html"));
+    }) 
+}
 
 server.listen(PORT, () => {
     console.log("server is runnig :" + PORT);
